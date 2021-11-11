d31a1af8a650f50dcdad8d46650c96c21e756752
Roy A Vanegas <roy@thecodeeducators.com>
2021-11-11 14:45:24 -0500

    Add example of the copy task

---
+++ javascript/gulp-refresher/the-copy-task/README.md
@@ -0,0 +1,3 @@
+Image of chipmunk in this example:
+
+https://www.flickr.com/photos/pmillera4/36680960172/in/photolist-XTnvvN-cAJtCb-TxKWKq-pQxHCa-Vr164e-cAJuhS-af6rsx-NM1EsK-af9ei7-bUS2x4-d6ZKbE-Ng129B-9sDBeE-6Wtre5-2axrnh-s4befD-VMuPUQ-f1rZSZ-Nse67J-tpoFbF-6s9fPs-NM1F2F-T6ynSZ-bUS1X6-5BtHUE-nNWVXD-DhbqVp-FBgzNp-ytUS8t-23vQC7y-af9aou-sKK67-gp9Le-d1Z9Zb-bUS3na-5htuk-Nse5C7-5htuh-ujqtr8-amEAwT-5htuj-5i69Ug-246DZYg-NU6hax-ncvDzp-2b92KKU-nR31P2-Xxr9eW-YipVUZ-gP25BW
---
+++ javascript/gulp-refresher/the-copy-task/gulpfile.js
@@ -0,0 +1,29 @@
+const { src, dest } = require(`gulp`);
+
+let paths = {
+    dev: `dev`,
+    html: `dev/html`,
+    styles: `dev/styles`,
+    js: `dev/js`,
+    images: `dev/img`
+};
+
+let copyUnprocessedAssets = () => {
+    return src([
+        `${paths.dev}/*.*`,     // Source all files,
+        `${paths.dev}/**`,      // and all folders,
+        `!${paths.html}/`,      // but don’t copy the HTML folder,
+        `!${paths.html}/*.*`,   // and any files in it,
+        `!${paths.html}/**`,    // and any of its sub folders;
+        `!${paths.images}/`,    // same for the images folder,
+        `!${paths.images}/*.*`, // its files,
+        `!${paths.images}/**`,  // and its sub folders;
+        `!${paths.js}/`,        // and again for the JavaScript folder,
+        `!${paths.js}/*.*`,     // its files,
+        `!${paths.js}/**`,      // and its sub folders;
+        `!${paths.styles}/**`   // and, finally, ignore the Sass/CSS sub folders.
+    ], {dot: true})
+        .pipe(dest(`prod`));
+};
+
+exports.copyUnprocessedAssets = copyUnprocessedAssets;
