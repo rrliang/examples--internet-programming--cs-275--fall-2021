695c14a90c587e4e00eb6890761d4f778f85d27c
Roy A Vanegas <roy@thecodeeducators.com>
2021-11-16 08:27:06 -0500

    Replace allElements.length with allNodes

--- javascript/examples-served-via-gulp/examples/print-out-the-name-of-every-element-in-a-tree/js/app.js
+++ javascript/examples-served-via-gulp/examples/print-out-the-name-of-every-element-in-a-tree/js/app.js
@@ -1,10 +1,11 @@
 window.onload = () => {
     let allElements = document.querySelectorAll(`*`),
-        message = document.getElementById(`message`);
+        message = document.getElementById(`message`),
+        allNodes = allElements.length;
 
     message.innerHTML = `<ul>`;
 
-    for (let i = 0; i < allElements.length; i++) {
+    for (let i = 0; i < allNodes; i++) {
         message.innerHTML += `<li><code>${allElements[i].nodeName}</code></li>`;
     }
 
